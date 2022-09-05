ed741653c541b62df4c59547bc3ed45e7d52da56
kototap <106796386+kototap@users.noreply.github.com>
2022-09-05 03:12:26 +0000

    [Add]devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,42 @@
 Rails.application.routes.draw do
+  
+  root to: 'homes#top'
+  get 'about' => 'homes#about'
+
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+  }
+
+  devise_for :users, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
+
+
+  namespace :admin do
+    # get '' => 'homes#top'
+
+    resources :tags, only: [:index, :create, :edit, :update, :destroy]
+    resources :users, only: [:index, :show, :edit, :update, :destroy]
+    resources :posts,only: [:destroy] do
+      resources :post_comments, only: [:destroy]
+    end
+  end
+
+
+  scope module: :public do
+    # root to: 'homes#top'
+    # get 'about' => 'homes#about'
+
+    resources :users, only: [:show, :edit, :update, :destroy] do
+      get '/unsubscribe' => 'users#unsubscribe'
+    end
+    resources :posts do
+      resources :bookmarks, only: [:index, :create, :destroy]
+      resources :post_comments, only: [:create, :destroy]
+    end
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20220904103758_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.integer :user_id, null: false
+      t.integer :tag_id, null: false
+      t.string :title, null: false
+      t.string :body, null: false
+
+      t.timestamps
+    end
+  end
+end
