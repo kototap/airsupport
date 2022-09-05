373d75cf26283685c7fce97fad8c446a101e64df
kototap <106796386+kototap@users.noreply.github.com>
2022-09-05 12:04:41 +0000

    [Add]user/show,edit,unsubscribe

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,18 +1,37 @@
 class Public::UsersController < ApplicationController
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "更新が完了しました！"
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
   end
 
   def unsubscribe
+    @user = current_user
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image, :email)
   end
 
 end
