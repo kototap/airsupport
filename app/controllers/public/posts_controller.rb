e7916abb4bc22b12ae3113c6e0cb8fcf7ad00301
kototap <106796386+kototap@users.noreply.github.com>
2022-09-06 03:42:46 +0000

    [Add]新規投稿画面の追加/Adminログイン機能/タグ機能追加

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,24 +1,41 @@
 class Public::PostsController < ApplicationController
-  
+
   def new
+    @post = Post.new
   end
-  
+
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to post_path(@post.id)
+    else
+      render 'new'
+    end
   end
-  
+
   def index
   end
-  
+
   def show
+    @post = Post.find(params[:id])
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :post_image)
+  end
+
+
 end
