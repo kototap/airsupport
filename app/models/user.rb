94c1b5297a5044609f595ba82937afc639934b0d
kototap <106796386+kototap@users.noreply.github.com>
2022-09-07 12:56:55 +0000

    [Update]ゲストログインのファイル変更/ransackのインストール

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
-      user.name = "guestuser"
+      user.name = "ゲストユーザー"
     end
   end
 
