bbbb16bb9b568c18b0b18d0a8b6fb9efd40e9719
kototap <106796386+kototap@users.noreply.github.com>
2022-09-15 08:57:14 +0000

    [Add]コメントとブックマークの非同期通信化

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -2,19 +2,21 @@ class Public::PostCommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    post = Post.find(params[:post_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.post_id = post.id
-    if comment.save
-      redirect_to post_path(post), notice: "コメントを投稿しました。"
+    @post = Post.find(params[:post_id])
+    @post_comment = current_user.post_comments.new(post_comment_params)
+    @post_comment.post_id = @post.id
+    if @post_comment.save
+      render :comments
     else
       redirect_back fallback_location: post
     end
   end
 
   def destroy
-    PostComment.find(params[:id]).destroy
-    redirect_to post_path(params[:post_id])
+    @post = Post.find(params[:post_id])
+    @post_comment = PostComment.find(params[:id])
+    @post_comment.destroy
+    render :comments
   end
 
 
