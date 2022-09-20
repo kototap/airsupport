94aa722eda4ee4830e97aa5e2578ccc2ceb38d0e
kototap <106796386+kototap@users.noreply.github.com>
2022-09-20 06:15:13 +0000

    [Add]部分テンプレート

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,6 @@ class User < ApplicationRecord
   has_many :post_comments, dependent: :destroy
 
     validates :name, presence: true, length: { maximum: 10 }
-    validates :introduction, presence: true, length: { maximum: 20 }
     validates :email, presence: true
 
 
