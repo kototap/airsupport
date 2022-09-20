7565aba5e029700fe273ae5af16ae5c8021061a0
kototap <106796386+kototap@users.noreply.github.com>
2022-09-20 14:26:06 +0000

    [Add]RSpec/Postモデル

---
+++ spec/factories/post.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :post do
+    title { Faker::Lorem.characters(number:5) }
+    body { Faker::Lorem.characters(number:10) }
+    airport { Faker::Lorem.characters(number:5) }
+    address { Faker::Address.full_address }
+    user
+    tag
+  end
+end
