00f8916f630ac346b8f1bf4a6be7a2e9da586d06
kototap <106796386+kototap@users.noreply.github.com>
2022-09-21 03:29:47 +0000

    [Add]RSpec/投稿機能

--- spec/system/post_spec.rb
+++ spec/system/post_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe "Posts", type: :request do
   end
 
   describe 'Postのテスト' do
-    let!(:post) { create(:post, title: 'hoge', body: 'hogehoge', airport: 'hoge') }
+    let!(:post) { create(:post, title: 'hoge', body: 'hogehoge', airport: 'fuga') }
 
     describe 'トップ画面(root_path)のテスト' do
       before do
@@ -48,7 +48,28 @@ RSpec.describe "Posts", type: :request do
   	    expect(page).to have_button '下書きに保存'
   	  end
   	 end
-  	 context 
+  	 context '投稿処理に関するテスト' do
+  	  it '投稿に成功しサクセスメッセージが表示されるか' do
+  	    fill_in 'post[title]', with: Faker::Lorem.characters(number:5)
+  	    fill_in 'post[body]', with: Faker::Lorem.characters(number:10)
+  	    fill_in 'post[airport]', with: Faker::Lorem.characters(number:5)
+  	    find("#post_tag_id").find("option[value='1']").select_option
+  	    fill_in 'post[address]', with: Faker::Address.full_address
+  	    click_button '投稿する'
+  	    expect(page).to have_content '投稿しました。'
+  	    expect(current_path).to eq(post_path(Post.last))
+  	  end
+  	  it '投稿に失敗する' do
+  	    click_button '投稿する'
+  	    expect(page).to have_content 'エラーが発生しました'
+  	    expect(current_path).to eq(posts_path)
+  	  end
+  	  it '下書き保存に成功しサクセスメッセージが表示されるか' do
+  	    click_button '下書きに保存'
+  	    expect(page).to have_content '下書きを保存しました。'
+  	    expect(current_path).to eq(user_path(@user))
+  	  end
+  	 end
   	end
 
 
@@ -60,7 +81,52 @@ RSpec.describe "Posts", type: :request do
         it 'posts_pathが"/posts"であるか' do
          expect(current_path).to eq('/posts')
         end
+        it  '投稿の情報、リンクが表示されているか' do
+          Post.new
+          expect(page).to have_link "hoge", href: post_path(Post.last)
+          expect(page).to have_link "hogehoge", href: post_path(Post.last)
+          expect(page).to have_link Post.last.tag.name, href: search_index_path(tag_id: Post.last.tag.id)
+          expect(page).to have_link "fuga", href: search_index_path(airport: "fuga")
+          expect(page).to have_link Post.last.user.name, href: user_path(Post.last.user)
+        end
       end
     end
+
+    describe '投稿詳細画面のテスト' do
+      before do
+        Post.new
+        visit post_path(Post.last)
+      end
+      context '投稿の詳細情報、リンクの確認' do
+        it 'post_pathが"/posts/Post.id"であるか' do
+          expect(current_path).to eq("/posts/#{Post.last.id}")
+        end
+        it '投稿の内容、リンクが反映されているか' do
+          expect(page).to have_content 'hoge'
+          expect(page).to have_content 'hogehoge'
+          expect(page).to have_link Post.last.tag.name, href: search_index_path(tag_id: Post.last.tag.id)
+          expect(page).to have_link "fuga", href: search_index_path(airport: "fuga")
+          expect(page).to have_link Post.last.user.name, href: user_path(Post.last.user)
+        end
+      end
+      context 'コメントの確認' do
+        it 'コメントフォームがあるか' do
+          expect(page).to have_field 'post_comment[comment]'
+          expect(page).to have_button '送信する'
+        end
+        it 'コメントがない時の表示' do
+          post_comments = PostComment.new
+          unless Post.last.post_comments.presence
+            expect(page).to have_content 'まだコメントがありません。'
+          end
+        end
+      end
+      context 'ブックマークの確認' do
+        it 'ブックマークの件数が表示されているか' do
+          expect(page).to have_link "#{Post.last.bookmarks.count}"
+        end
+      end
+    end
+
   end
 end
