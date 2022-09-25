a54a14b0fa16cde67ae8aca9656ae4f21f619260
kototap <106796386+kototap@users.noreply.github.com>
2022-09-25 23:00:30 +0000

    [Fix]投稿一覧ブックマーク順を週間から累計に変更

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,9 +1,12 @@
 class Public::HomesController < ApplicationController
   def top
     @posts = Post.release.latest.limit(3)
-    @ranks = Post.release.find(Bookmark.group(:post_id).where(created_at: Time.current.all_week).order("count(post_id) desc").limit(3).pluck(:post_id))
+    @ranks = Post.release.find(Bookmark.group(:post_id).order("count(post_id) desc").limit(3).pluck(:post_id))
   end
 
   def about
   end
 end
+
+
+    # @ranks = Post.release.find(Bookmark.group(:post_id).where(created_at: Time.current.all_week).order("count(post_id) desc").limit(3).pluck(:post_id))
