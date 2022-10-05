4851eff8732ff2960c46e61359e49f27363a3d27
kototap <106796386+kototap@users.noreply.github.com>
2022-10-05 15:17:12 +0000

    [Modify]Headerレイアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :set_search
 
   def set_search
-    @search = Post.ransack(params[:q])
-    @posts = @search.result.order(created_at: :desc).page(params[:page])
+    @search = Post.left_joins(:tags).release.ransack(params[:q])
+    posts = @search.result(distinct: true)
+    @posts = posts.latest.page(params[:page])
   end
 end
