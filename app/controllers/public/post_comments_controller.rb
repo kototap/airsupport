74628fe9bf6ab1440224bbd621338a43fbbf732d
kototap <106796386+kototap@users.noreply.github.com>
2022-10-10 06:35:41 +0000

    [Fix]インデント

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -23,7 +23,9 @@ class Public::PostCommentsController < ApplicationController
 
 
   private
-    def post_comment_params
-      params.require(:post_comment).permit(:comment)
-    end
+
+  def post_comment_params
+    params.require(:post_comment).permit(:comment)
+  end
+
 end
